15715f58cd1d54daa603bf057c8f6828fb46e2c5
Sergey <sergey.paramonov@phystech.edu>
2016-12-07 23:08:02 +0100

    started working on a wrapper

--- post-processing-asp/src/main.cpp
+++ post-processing-asp/src/main.cpp
@@ -4,7 +4,11 @@
 using namespace std;
 
 int main(){
-  cout << "Hello World\n";
-  system("touch test2");
+  string miner_name = "bin/eclat";
+  string dataset_name = "datasets/itemsets/mushooms.dat";
+  string output = "tmp/tmp_output";
+  string command = miner_name + " " + dataset_name +" > " + output + "\n";
+  cout << "executing the command " << command;
+  system(command.c_str());
   return 0;
 }
