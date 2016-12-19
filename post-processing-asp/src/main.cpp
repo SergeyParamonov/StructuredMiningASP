1c5afd88306daba8ed22006cb16c8689ac9ecb85
Sergey <sergey.paramonov@phystech.edu>
2016-12-19 23:53:31 +0100

    made a folder for processed patterns

--- post-processing-asp/src/main.cpp
+++ post-processing-asp/src/main.cpp
@@ -4,7 +4,7 @@
 int main(){
     string tmp_file ="tmp/tmp_output";
     unique_ptr<Miner> miner = pick_miner("itemset");
-    miner->run_solver("datasets/itemsets/mushrooms.txt",tmp_file);
+    miner->run_solver("datasets/itemsets/mushroom.txt",tmp_file);
     vector<unique_ptr<Pattern>> patterns = miner->parse_solver_output(tmp_file);
     miner->write_asp_patterns_to_file(move(patterns), "processed/mushroom_patterns");
     patterns.clear();
