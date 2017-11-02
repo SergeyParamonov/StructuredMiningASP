a3c091b1c1d561a8b0c444009601bdc813da3bd9
Sergey <sergey.paramonov@phystech.edu>
2017-11-02 22:41:34 +0100

    added wasp

--- post-processing-asp/src/asp_engine.cpp
+++ post-processing-asp/src/asp_engine.cpp
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 
 class ASP_Engine{
+  
+  private: 
+    enum Engine {clingo = 0, wasp = 1};
+    static const Engine config = wasp;
+
   public:
     ASP_Engine(){};
    ~ASP_Engine(){};
@@ -8,6 +13,7 @@ class ASP_Engine{
 
   void run_on_file(string data_filename, string output_filename, string pattern_type, string data_type){
     string program;
+    string command;
     bool is_testing_under_constraints = false;
     if (is_testing_under_constraints){
       program = "asp_encodings/"+data_type+"_"+pattern_type+"_under_constraints.asp";
@@ -15,7 +21,13 @@ class ASP_Engine{
     else{
       program = "asp_encodings/"+data_type+"_"+pattern_type+".asp";
     }
-    string command = "clingo " + program + " " + data_filename + " 0 > " + output_filename;     
+    if (ASP_Engine::config == wasp){
+      command = "./bin/gringo " + program + " " + data_filename + " | ./bin/wasp" + " 0 > " + output_filename;     
+      std::cout << command;
+    }
+    else{
+      command = "clingo " + program + " " + data_filename + " 0 > " + output_filename;     
+    }
     cout << "executing... " + command + "\n";
     system(command.c_str());
   }
